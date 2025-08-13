33242aa35874315d7626e030af0a694a8773e226
erick-roberto <roberto.002ggr@gmail.com>
2025-08-13 01:05:19 -0300

    update cardapio

--- src/pages/Cardapio.jsx
+++ src/pages/Cardapio.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import PizzaCard from "../components/PizzaCard";
+import "../index.css"; // garante que o CSS global seja carregado
 
 export default function Cardapio() {
   const [pizzas, setPizzas] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/pizzas").then((res) => setPizzas(res.data));
+    axios.get("http://localhost:5000/pizzas")
+      .then((res) => setPizzas(res.data))
+      .catch((err) => console.error("Erro ao buscar pizzas:", err));
   }, []);
 
   const handleAdd = (pizza) => {
@@ -14,11 +17,17 @@ export default function Cardapio() {
   };
 
   return (
-    <div style={{padding: 20}}>
-      <h2>Cardápio</h2>
-      {pizzas.map((pizza) => (
-        <PizzaCard key={pizza.id} pizza={pizza} onAdd={handleAdd} />
-      ))}
+    <div>
+      <h2 style={{ padding: "20px 20px 0 20px" }}>🍕 Cardápio</h2>
+      <div className="pizzas-grid">
+        {pizzas.length > 0 ? (
+          pizzas.map((pizza) => (
+            <PizzaCard key={pizza.id} pizza={pizza} onAdd={handleAdd} />
+          ))
+        ) : (
+          <p style={{ padding: 20 }}>Nenhuma pizza disponível no momento.</p>
+        )}
+      </div>
     </div>
   );
 }
